c2ef47539e6feab644c431ae5d7e7bfc2d400ee9
Dat <193557964+cupeedrl@users.noreply.github.com>
2026-05-18 23:29:37 +0700

    Add Java program to calculate sum of first N integers

---
+++ J01002 - TÍNH TỔNG N SỐ NGUYÊN DƯƠNG ĐẦU TIÊN.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CodeptitUp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            System.out.println(SumToN(n));
+        }
+    }
+
+    static long SumToN(long n)
+    {
+        return n*(n+1)/2;
+    }
+}
