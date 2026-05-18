a3a736b480be2c657cbfb1711ad2efc31ed71d21
Dat <193557964+cupeedrl@users.noreply.github.com>
2026-05-18 23:48:58 +0700

    Create J04001 - KHAI BÁO LỚP POINT.java

---
+++ J04001 - KHAI BÁO LỚP POINT.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import static java.lang.Math.*;
+class Point{
+
+    private double x,y;
+
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+
+    public Point (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    double distance(Point p) {
+        return sqrt(pow(this.x - p.x, 2) + pow(this.y - p.y, 2));
+    }
+
+
+    @Override
+    public String toString(){
+        return x +" "+ y;
+    }
+}
+
+public class Up {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            Point a = new Point(x,y);
+            x = sc.nextDouble();
+            y = sc.nextDouble();
+            Point b = new Point(x, y);
+            System.out.printf("%.4f\n", a.distance(b));
+        }
+    }
+}
