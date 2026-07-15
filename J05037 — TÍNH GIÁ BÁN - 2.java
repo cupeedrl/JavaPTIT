f276df908c7735c86145f34b35fb143615591530
Dat <193557964+cupeedrl@users.noreply.github.com>
2026-07-15 11:56:02 +0700

    Create J05037 — TÍNH GIÁ BÁN - 2.java

---
+++ J05037 — TÍNH GIÁ BÁN - 2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Expense {
+    private String name,unit,id;
+    private long buy,sell,delivery,amount,quantity;
+    private static int i = 1;
+
+    public Expense(String name, String unit, long buy, long quantity) {
+        this.id = String.format("MH%02d", i++);
+        this.name = name;
+        this.unit = unit;
+        this.buy = buy;
+        this.quantity = quantity;
+
+        this.delivery = Math.round(buy*quantity* 0.05f);
+        this.amount = buy*quantity + delivery;
+        this.sell = Math.round(amount* 1.02f)/quantity;
+        this.sell = (long)Math.ceil(sell / 100.0) * 100;
+    }
+
+    public long getSell() {
+        return sell;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s %s %s %d %d %d",id,name,unit,delivery,amount,sell);
+    }
+}
+
+public class J05036 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+
+        List<Expense> list = new ArrayList<>();
+
+        for (int i = 0; i < t; i++) {
+            String name = sc.nextLine();
+            String unit = sc.nextLine();
+            long buy = Long.parseLong(sc.nextLine());
+            long number = Long.parseLong(sc.nextLine());
+
+            list.add(new Expense(name,unit,buy,number));
+        }
+        list.sort(Comparator.comparing(Expense::getSell).reversed());
+        for (Expense x : list){
+            System.out.println(x);
+        }
+    }
+}
