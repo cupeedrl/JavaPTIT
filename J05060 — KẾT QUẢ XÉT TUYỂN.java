b752b12cec12b54f408607f705ffe64279b52f99
Dat <193557964+cupeedrl@users.noreply.github.com>
2026-07-15 12:03:59 +0700

    Create J05060 — KẾT QUẢ XÉT TUYỂN.java

---
+++ J05060 — KẾT QUẢ XÉT TUYỂN.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Worker {
+    private String id, name, birth;
+    private double theory, practice;
+    private int age, avg;
+    private String rank;
+
+    private static int cnt = 1;
+
+    public Worker(String name, String birth, double theory, double practice) {
+        this.id = String.format("PH%02d", cnt++);
+        this.name = name;
+        this.birth = birth;
+        this.theory = theory;
+        this.practice = practice;
+
+        this.age = 2021 - Integer.parseInt(birth.substring(6));
+
+        double bonus = calBonus(theory, practice);
+
+        this.avg = (int)Math.round((theory + practice) / 2 + bonus);
+
+        if(this.avg > 10) this.avg = 10;
+
+        this.rank = calRank(avg);
+    }
+
+    public double calBonus(double theory, double practice){
+        if(theory >= 8 && practice >= 8) return 1;
+        if(theory >= 7.5 && practice >= 7.5) return 0.5;
+        return 0;
+    }
+
+    public String calRank(int avg){
+        if(avg < 5) return "Truot";
+        if(avg <= 6) return "Trung binh";
+        if(avg == 7) return "Kha";
+        if(avg == 8) return "Gioi";
+        return "Xuat sac";
+    }
+
+    @Override
+    public String toString(){
+        return id + " " + name + " " + age + " " + avg + " " + rank;
+    }
+}
+
+public class J05060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        List<Worker> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            String name = sc.nextLine();
+            String birth = sc.nextLine();
+            double theory = Double.parseDouble(sc.nextLine());
+            double practice = Double.parseDouble(sc.nextLine());
+
+            list.add(new Worker(name, birth, theory, practice));
+        }
+
+        for(Worker x : list){
+            System.out.println(x);
+        }
+    }
+}
