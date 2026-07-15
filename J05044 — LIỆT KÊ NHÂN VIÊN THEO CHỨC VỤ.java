25022f5e81af6c748fcba4262592c0789648c2a0
Dat <193557964+cupeedrl@users.noreply.github.com>
2026-07-15 11:58:13 +0700

    Create J05044 — LIỆT KÊ NHÂN VIÊN THEO CHỨC VỤ.java

---
+++ J05044 — LIỆT KÊ NHÂN VIÊN THEO CHỨC VỤ.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Emp {
+    private String id, name, pos;
+    private long salary, days;
+
+    private long monthSalary, allowance, pre, remain;
+
+    private static int cnt = 1;
+
+    public Emp(String name, String pos, long salary, long days) {
+        this.id = String.format("NV%02d", cnt++);
+        this.name = name;
+        this.pos = pos;
+        this.salary = salary;
+        this.days = days;
+
+        this.monthSalary = salary * days;
+        this.allowance = support(pos);
+        this.pre =takePrev(monthSalary,allowance);
+        this.remain = monthSalary +allowance - pre;
+    }
+
+    public String getPos() {
+        return pos;
+    }
+
+    public static long takePrev(long monthSalary, long allowance){
+        double amount = (monthSalary + allowance) * 2.0 / 3;
+        if(amount < 25000)
+            return Math.round(amount / 1000) * 1000;
+        return 25000;
+    }
+
+    public static long support(String pos){
+        if(pos.equals("GD")) return 500;
+        if(pos.equals("PGD")) return 400;
+        if(pos.equals("TP")) return 300;
+        if(pos.equals("KT")) return 250;
+        else return 100;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + allowance  + " " + monthSalary + " " + pre + " " + remain;
+    }
+}
+
+public class J05044 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        List<Emp> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.nextLine();
+            String pos = sc.nextLine();
+            long salary = Long.parseLong(sc.nextLine());
+            long days = Long.parseLong(sc.nextLine());
+            list.add(new Emp(name,pos,salary,days));
+        }
+        String query = sc.nextLine();
+
+        for (Emp x : list) {
+            if(query.equals(x.getPos()))
+                System.out.println(x);
+        }
+    }
+}
